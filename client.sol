e3569d80e770f572fd06e157356ccbb397419fdd
Guillaume Lauzier <55703540+guillaumelauzier@users.noreply.github.com>
2023-07-11 11:51:18 +0200

    Update client.sol

--- client.sol
+++ client.sol
@@ -1,1 +1,30 @@
+pragma solidity ^0.8.0;
 
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+contract MyContract is ChainlinkClient {
+    using Chainlink for Chainlink.Request;
+
+    uint256 public volume;
+
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee;
+
+    constructor() {
+        setPublicChainlinkToken();
+        oracle = 0x123...; // This should be the address of the oracle
+        jobId = "abc123..."; // This should be the job ID
+        fee = 0.1 * 10 ** 18; // This is the fee (0.1 LINK in this case)
+    }
+
+    function requestData() public returns (bytes32 requestId) {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        request.add("get", "http://api.example.com/data"); // This should be your off-chain API URL
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+
+    function fulfill(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId) {
+        volume = _volume;
+    }
+}
