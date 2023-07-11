908eff0ea02b402ae831b5aea5aad64ea6a53c86
Guillaume Lauzier <55703540+guillaumelauzier@users.noreply.github.com>
2023-07-11 11:48:17 +0200

    Update src.sol

--- src.sol
+++ src.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT2 is ERC721 {
+    struct Contribution {
+        address contributor;
+        uint percentage;
+    }
+
+    mapping(uint => Contribution[]) public contributions;
+    
+    constructor() ERC721("NFT2", "NFT") {}
+    
+    function mintNFT(address recipient, uint tokenId) public {
+        _mint(recipient, tokenId);
+    }
+
+    function addContribution(uint tokenId, address contributor, uint percentage) public {
+        contributions[tokenId].push(Contribution(contributor, percentage));
+    }
+
+    function getContributions(uint tokenId) public view returns (Contribution[] memory) {
+        return contributions[tokenId];
+    }
+}
 
